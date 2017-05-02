cd2da1e78a48730a8ebc5bab3344348e1de3250e
Lukasz Szczesniak <veiner87@gmail.com>
2017-05-02 22:22:49 +0200

    branch with master

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+var text = 'Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach.';
+var animal = 'Zielone słonie';
+// var animalUpperCased = animal.toUpperCase();
+
+var textCharsAfter = text.replace ('Papugi', animal.toUpperCase());
+
+console.log(textCharsAfter);
